dc439fe9ffe76947848aac8452ef5b72cd6bf4bc
Naresh Sharma <sharmanaresh272@gmail.com>
2022-08-11 13:01:54 +0530

    initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String apiKey = 'AIzaSyDSnGwJjU8GXIexV4sfH6N92B8yDvT6m7k';
+  static String appId = '1:778257549921:web:4338bb70c5157f86aaf820';
+  static String messagingSenderId = '778257549921';
+  static String projectId = 'flutter-chat-app-98aa0';
+
+  static const primaryColor = Color.fromRGBO(255, 153, 0, 1);
+}
